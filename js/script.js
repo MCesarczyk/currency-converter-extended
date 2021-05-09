cd6679f558837892a3a739be2b134c6e44614b5a
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-05-09 14:20:15 +0200

    sptlitted .js file info function according to ES6

--- js/script.js
+++ js/script.js
@@ -3,48 +3,53 @@
         console.log("Hello, programmer! Have a nice day :)");
     };
 
-    const calculate = (event) => {
-        event.preventDefault();
-        const pln = document.querySelector(".js-pln");
-        const eur = document.querySelector(".js-eur");
-        const usd = document.querySelector(".js-usd");
-        const bgn = document.querySelector(".js-bgn");
-        const hrk = document.querySelector(".js-hrk");
-        pln.focus();
-        let ratioEur = document.querySelector(".js-ratioEur");
-        let ratioUsd = document.querySelector(".js-ratioUsd");
-        let ratioBgn = document.querySelector(".js-ratioBgn");
-        let ratioHrk = document.querySelector(".js-ratioHrk");
-        let result = document.querySelector(".js-result");
-        let gain;
-        let currency;
-        if (eur.checked === true) {
-            gain = pln.value / ratioEur.value;
-            currency = " EUR";
-        } else if (usd.checked === true) {
-            gain = pln.value / ratioUsd.value;
-            currency = " USD";
-        } else if (bgn.checked === true) {
-            gain = pln.value / ratioBgn.value;
-            currency = " BGN";
-        } else if (hrk.checked === true) {
-            gain = pln.value / ratioHrk.value;
-            currency = " HRK";
-        } else {
-            result.value = "bida panie ..."
+    const checkCurrency = () => {
+        return checkedCurrency = document.querySelector(".js-radio:checked");
+    };
+
+    const checkCurrencyRatio = () => {
+        const ratioEur = document.querySelector(".js-ratioEur").value;
+        const ratioUsd = document.querySelector(".js-ratioUsd").value;
+        const ratioBgn = document.querySelector(".js-ratioBgn").value;
+        const ratioHrk = document.querySelector(".js-ratioHrk").value;
+        checkCurrency();
+        switch (checkedCurrency.value) {
+            case "EUR":
+                return chosenCurrencyRatio = ratioEur;
+            case "USD":
+                return chosenCurrencyRatio = ratioUsd;
+            case "BGN":
+                return chosenCurrencyRatio = ratioBgn;
+            case "HRK":
+                return chosenCurrencyRatio = ratioHrk;
         };
-        result.innerText = "Uzyskana kwota: " + gain.toFixed(2) + currency;
-    }
+    };
+
+    const calculate = () => {
+        const inputPln = document.querySelector(".js-pln").value;
+        return gain = inputPln / chosenCurrencyRatio;
+    };
+
+    const printResult = () => {
+        checkCurrency();
+        checkCurrencyRatio();
+        calculate();
+        const result = document.querySelector(".js-result");
+        result.innerText = `Uzyskana kwota: ${gain.toFixed(2)} ${checkedCurrency.value}`;
+    };
 
     const formReset = () => {
-        let result = document.querySelector(".js-result");
+        const result = document.querySelector(".js-result");
         result.innerText = "Dziękujemy, zapraszamy ponownie :)";
     };
 
     const init = () => {
         const form = document.querySelector(".js-form");
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            printResult();
+        });
         form.addEventListener("reset", formReset);
-        form.addEventListener("submit", calculate);
     };
 
     welcome();
